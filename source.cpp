b2528abe54fb44f7c205d499a2659804ec8571a8
unicornt <unicorntwin@outlook.com>
2022-03-04 06:52:18 +0000

    add a page

--- source.cpp
+++ source.cpp
@@ -3,6 +3,7 @@ using namespace std;
 const string null_str = "";
 const int func_num = 10000;
 enum {init, marked, demarked, compiling, optimizing, optimized};
+string stats[10] = {"init", "marked", "demarked", "compiling", "optimizing", "optimized"};
 map<string, int>sfiTable;
 int status[func_num];
 vector<int>slist[func_num]; // list of status
@@ -10,6 +11,12 @@ vector<string>rlist[func_num]; // list of reason(if have)
 vector<int>rtlist[func_num]; // list of times of running
 
 //////////////////////////////////////////////////////////
+
+void throw_error(string reason, string lines) {
+    cout << "error: " << reason << std::endl;
+    cout << "input string is " << lines << std::endl;
+    exit(1);
+}
 
 void AnalyzeCode(string s) {
 
@@ -19,14 +26,23 @@ string GetSFI(string s) {
     string sfi = null_str;
     size_t p = s.find("sfi");
     if(p == string::npos) {
-        // error
-        cout << "error: no sfi" << std::endl;
-        exit(1);
+        // input string is [marking dependent code 0x003c00052481 (0x003c0833c449 <SharedFunctionInfo process>)
+        p = s.find("0x");
+        if(p == string::npos) return null_str;
+        sfi = s.substr(p, 14);
+        return sfi;
     }
     p += 3;
     while(s[p] == ' ' || s[p] == '=') p++;
-    while(s[p] != ')') {
+    while(isalnum(s[p]) || s[p] == 'x') {
         sfi += s[p++];
+    }
+    int delt = 14 - sfi.length();
+    if(delt > 0) {
+        sfi = "0x" + string(delt, '0') + sfi.substr(2);
+    }
+    else if(delt < 0) {
+        throw_error("delt < 0", s);
     }
     cout << "sfi " << sfi << std::endl;
     return sfi;
@@ -58,11 +74,13 @@ string GetReason(string s) {
     string reason = null_str;
     size_t p = s.find("reason");
     if(p == string::npos) {
-        cout << "error: no reason" << std::endl;
-        exit(1);
+        if(s.find("ERROR") != string::npos) return "ERROR";
+        throw_error("no reason", s);
     }
+    cout << p << std::endl;
     p += 8;
-    while(s[p] != ')') {
+    while(isalpha(s[p]) || s[p] == ' ') {
+        // cout << s[p] << endl;
         reason += s[p++];
     }
     return reason;
@@ -71,6 +89,7 @@ string GetReason(string s) {
 
 void Analyze(string s) {
     string sfi;
+    int sn;
     if(s[0] != '[') AnalyzeCode(s);
     else {
         switch (s[1])
@@ -79,21 +98,21 @@ void Analyze(string s) {
         case 'm':
             // marking XXXXXXX for optimized recompilation
             // marking XXXXXXX for deoptimization
-            // get sfi of the function & save it if never meet it before
             sfi = GetSFI(s);
             // get sfi of the function & save it if never meet it before init it as "init"
             // mark it as "marking"
             // deoptimization: get last optimized
             if(sfi == null_str) {
                 // error
-                cout << "error: no sfi" << std::endl;
+                throw_error("no sfi code", s);
                 exit(1);
             }
             if(sfiTable.find(sfi) == sfiTable.end()) {
                 // new sfi
                 NewSFI(sfi);
             }
-            int sn = sfiTable[sfi];
+            sn = sfiTable[sfi];
+            cout << sfi << ' ' << sn << std::endl;
             // mark it as "marked/demarked"
             ChangeStatus(sn, CheckTar(s), GetReason(s));
             break;
@@ -101,7 +120,12 @@ void Analyze(string s) {
             //compiling method XXXXXXX using TurboFan OSR
             // get sfi of the function & mark it as "compiling"
             sfi = GetSFI(s);
-            int sn = sfiTable[sfi];
+            if(sfi == null_str) {
+                // error
+                throw_error("no sfi code", s);
+                exit(1);
+            }
+            sn = sfiTable[sfi];
             if(s[4] == 'i') {
                 ChangeStatus(sn, compiling);
             }
@@ -115,25 +139,36 @@ void Analyze(string s) {
             // optimizing 
             // mark it as "optimizing"
             sfi = GetSFI(s);
-            int sn = sfiTable[sfi];
+            if(sfi == null_str) {
+                // error
+                throw_error("no sfi code", s);
+                exit(1);
+            }
+            sn = sfiTable[sfi];
             ChangeStatus(sn, optimizing);
             break;
         case 'b':
             // bailout 
             // record reason
             sfi = GetSFI(s);
-            int sn = sfiTable[sfi];
+            if(sfi == null_str) {
+                // error
+                throw_error("no sfi code", s);
+                exit(1);
+            }
+            sn = sfiTable[sfi];
             ChangeStatus(sn, init, GetReason(s));
             break;
         default:
             cout << "unsupport sentence" << std::endl;
+            cout << "    " << s.substr(0, 10) << "..." << std::endl;
             break;
         }
     }
 }
 int main() {
     ifstream fp;
-    fp.open("log/log.txt", ios::in);
+    fp.open("log/log-with-bytecode.txt", ios::in);
     string ls;
     while(!fp.eof() && fp.good()) {
         getline(fp, ls);
@@ -145,7 +180,7 @@ int main() {
         cout << it->first << " : " << std::endl;
         int sn = it->second;
         for(int i = 0; i < slist[sn].size(); i++) {
-            cout << slist[sn][i] << ' ';
+            cout << stats[slist[sn][i]] << ' ';
         }
         cout << std::endl;
     }
